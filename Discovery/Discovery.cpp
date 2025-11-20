5c52decf5445ce3ddcecf196073d16286b98584c
trsohankumar <sohankumartr@gmail.com>
2025-11-20 18:22:00 +0100

    Add: Created a vector to store discovered peers

--- Discovery/Discovery.cpp
+++ Discovery/Discovery.cpp
@@ -5,10 +5,10 @@
 #include "Discovery.h"
 
 Agora::Discovery::Discovery(uuids::uuid pEntityId, std::string pIpAddress, const uint16_t pPort, std::string pBroadcastAddress, const uint16_t pBroadcastPort)
-    :vDiscoveryMessage(std::move(pEntityId), std::move(pIpAddress), pPort), vBroadcastAddress(std::move(pBroadcastAddress)), vBroadcastPort(pBroadcastPort) {
+    :vNodeInfo(std::move(pEntityId), std::move(pIpAddress), pPort), vBroadcastAddress(std::move(pBroadcastAddress)), vBroadcastPort(pBroadcastPort) {
 }
 
-void Agora::Discovery::Broadcast() const{
+void Agora::Discovery::Broadcast(std::vector<Agora::Node>& pDiscoveredNodes, std::mutex& pDiscoveredNodesMutex) const{
     spdlog::info("Broadcast started");
 
     int sock = ::socket(AF_INET, SOCK_DGRAM, 0);
@@ -31,7 +31,7 @@ void Agora::Discovery::Broadcast() const{
 
     while (true) {
         
-        nlohmann::json discoveryMessage = vDiscoveryMessage.toJson();
+        nlohmann::json discoveryMessage = vNodeInfo.toJson();
         const std::string payload = discoveryMessage.dump();
 
         spdlog::debug("Broadcasting {} bytes to {}:{}", payload.size(), vBroadcastAddress, vBroadcastPort);
@@ -43,6 +43,14 @@ void Agora::Discovery::Broadcast() const{
             spdlog::warn("Broadcast sendto failed: {}", std::strerror(errno));
         }
 
+        {
+            std::lock_guard<std::mutex> lock(pDiscoveredNodesMutex);
+            // can be removed later
+            spdlog::info("Currently discovered nodes");
+            for (const auto node : pDiscoveredNodes) {
+                spdlog::info("id: {}", uuids::to_string(node.getNodeIdentifier()));
+            }
+        }
         std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 
@@ -93,15 +101,28 @@ void Agora::Discovery::Listen(std::vector<Agora::Node>& pDiscoveredNodes, std::m
 
         try {
             nlohmann::json discoveredMessage = nlohmann::json::parse(buf.c_str(), buf.c_str() + payloadSize);
-            Agora::Message::Discovery discoveredNode;
-            discoveredNode.fromJson(discoveredMessage);
+            Agora::Node discoveredNode(discoveredMessage);
 
-            if (discoveredNode.uEntityId == vDiscoveryMessage.uEntityId) continue;
+            if (discoveredNode.getNodeIdentifier() == vNodeInfo.getNodeIdentifier()) continue;
 
-            spdlog::info("Discovery: <Id: {}, Ip: {}, Port: {}> discovered <Id: {}, Ip: {}, Port: {}>",
-                uuids::to_string(vDiscoveryMessage.uEntityId), vDiscoveryMessage.uIpAddress, vDiscoveryMessage.uPort,
-                uuids::to_string(discoveredNode.uEntityId), discoveredNode.uIpAddress, discoveredNode.uPort);
-
+            {
+                std::lock_guard<std::mutex> lock(pDiscoveredNodesMutex);
+                
+                bool alreadyExists = false;
+                for (const auto node : pDiscoveredNodes) {
+                    if (node.getNodeIdentifier() == discoveredNode.getNodeIdentifier()) {
+                        alreadyExists = true;
+                        break;
+                    }
+                }
+                
+                if (!alreadyExists) {
+                    pDiscoveredNodes.push_back(discoveredNode);
+                    spdlog::info("Discovery: <Id: {}, Ip: {}, Port: {}> discovered <Id: {}, Ip: {}, Port: {}>",
+                        uuids::to_string(vNodeInfo.getNodeIdentifier()), vNodeInfo.getNodeIpAddress(), vNodeInfo.getNodePort(),
+                        uuids::to_string(discoveredNode.getNodeIdentifier()), discoveredNode.getNodeIpAddress(), discoveredNode.getNodePort());
+                }
+            }
         } catch (const std::exception& e) {
             spdlog::warn("Failed to parse incoming JSON: {}", e.what());
         }
--- Node/Node.h
+++ Node/Node.h
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <spdlog/spdlog.h>
 #include <uuid.h>
+#include <nlohmann/json.hpp>
 
 #include "../Utility/UuidGeneration.h"
 
@@ -13,11 +14,14 @@ class Node
 {
 
 public:
-                Node();
-                Node(uuids::uuid pNodeIdentifier,std::string pIpAddress, uint16_t pPort);
-    uuids::uuid getNodeIdentifier()                                                         const;
-    std::string getNodeIpAddress()                                                          const;
-    uint16_t    getNodePort()                                                               const;
+                    Node();
+                    Node(uuids::uuid pNodeIdentifier,std::string pIpAddress, uint16_t pPort);
+                    Node(const nlohmann::json& pJsonNode);
+    uuids::uuid     getNodeIdentifier()                                                         const;
+    std::string     getNodeIpAddress()                                                          const;
+    uint16_t        getNodePort()                                                               const;
+    nlohmann::json  toJson()                                                                    const;
+    void            fromJson(const nlohmann::json& pJsonNode);
 
 private:
     uuids::uuid vNodeIdentifier;
