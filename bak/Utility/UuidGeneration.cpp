0bc03f7990935a74056663a6e85d84f72691011f
trsohankumar <sohankumartr@gmail.com>
2025-12-22 19:35:45 +0100

    del: old cpp code

--- bak/Utility/UuidGeneration.cpp
+++
@@ -1,8 +0,0 @@
-#include "UuidGeneration.h"
-
-uuids::uuid generateRandomUuid()
-{
-    std::mt19937 rng{std::random_device{}()};     
-    uuids::uuid_random_generator generator(rng);
-    return generator();
-}
